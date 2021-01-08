bdffecfab43f265953e08697f542096d1f84e006
luigiplatania94 <57711349+luigiplatania94@users.noreply.github.com>
2021-01-08 16:15:49 +0000

    Packaging

---
+++ AudioSplinePlugin/Intermediate/Build/Win64/UE4/Inc/AudioSplinePlugin/AudioSpline.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef AUDIOSPLINEPLUGIN_AudioSpline_generated_h
+#error "AudioSpline.generated.h already included, missing '#pragma once' in AudioSpline.h"
+#endif
+#define AUDIOSPLINEPLUGIN_AudioSpline_generated_h
+
+#define HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_SPARSE_DATA
+#define HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_RPC_WRAPPERS
+#define HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_RPC_WRAPPERS_NO_PURE_DECLS
+#define HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAAudioSpline(); \
+	friend struct Z_Construct_UClass_AAudioSpline_Statics; \
+public: \
+	DECLARE_CLASS(AAudioSpline, AAmbientSound, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/AudioSplinePlugin"), NO_API) \
+	DECLARE_SERIALIZER(AAudioSpline)
+
+
+#define HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_INCLASS \
+private: \
+	static void StaticRegisterNativesAAudioSpline(); \
+	friend struct Z_Construct_UClass_AAudioSpline_Statics; \
+public: \
+	DECLARE_CLASS(AAudioSpline, AAmbientSound, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/AudioSplinePlugin"), NO_API) \
+	DECLARE_SERIALIZER(AAudioSpline)
+
+
+#define HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AAudioSpline(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AAudioSpline) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAudioSpline); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAudioSpline); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAudioSpline(AAudioSpline&&); \
+	NO_API AAudioSpline(const AAudioSpline&); \
+public:
+
+
+#define HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAudioSpline(AAudioSpline&&); \
+	NO_API AAudioSpline(const AAudioSpline&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAudioSpline); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAudioSpline); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AAudioSpline)
+
+
+#define HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_PRIVATE_PROPERTY_OFFSET
+#define HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_19_PROLOG
+#define HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_PRIVATE_PROPERTY_OFFSET \
+	HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_SPARSE_DATA \
+	HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_RPC_WRAPPERS \
+	HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_INCLASS \
+	HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_PRIVATE_PROPERTY_OFFSET \
+	HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_SPARSE_DATA \
+	HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_RPC_WRAPPERS_NO_PURE_DECLS \
+	HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_INCLASS_NO_PURE_DECLS \
+	HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h_22_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> AUDIOSPLINEPLUGIN_API UClass* StaticClass<class AAudioSpline>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID HostProject_Plugins_AudioSplinePlugin_Source_AudioSplinePlugin_Public_AudioSpline_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
